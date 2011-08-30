5b145b8be9cc9245aa60e7355642bb36f7c951e8
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-08-30 12:40:59 +0200

    added json REST ws for lesson

--- grails-app/controllers/com/kgrodzicki/flashcards/grails/LessonController.groovy
+++ grails-app/controllers/com/kgrodzicki/flashcards/grails/LessonController.groovy
@@ -1,7 +1,12 @@
 package com.kgrodzicki.flashcards.grails
 
 import com.kgrodzicki.flashcards.grails.Lesson;
+import grails.converters.JSON
 
 class LessonController {
 	static scaffold = Lesson
+	
+	def showJson = {
+		render Lesson.list() as JSON
+	}
 }
