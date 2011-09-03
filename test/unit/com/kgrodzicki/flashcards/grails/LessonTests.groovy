a226415e35cc4d74684ba9216496093e2ea22dda
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-09-03 23:57:12 +0200

    updated packages for tests classes

---
+++ test/unit/com/kgrodzicki/flashcards/grails/LessonTests.groovy
@@ -0,0 +1,17 @@
+package com.kgrodzicki.flashcards.grails
+
+import grails.test.*
+
+class LessonTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
