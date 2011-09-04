fbb76ea0ee675b9f5fef4a74b2b516ae3e3d77a9
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-09-04 16:31:55 +0200

    removed init objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,16 +4,6 @@ import com.kgrodzicki.flashcards.grails.Lesson
 class BootStrap {
 
     def init = { servletContext ->
-		
-		def card = new Card(question: "front 1", answer: "back 1").save(failOnError: true)
-		def card2 = new Card(question: "front 2", answer: "back 2").save(failOnError: true)
-		
-		def lesson = new Lesson(name: "Test lesson")
-		
-		lesson.addToCards(card)
-		lesson.addToCards(card2)
-		
-		lesson.save(failOnError: true)
     }
     def destroy = {
     }
