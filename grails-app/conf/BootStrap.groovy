0276b8930c4a4360d2603302adcb7594cb2afaf4
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-09-04 20:31:11 +0200

    added user and rule

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,6 +4,18 @@ import com.kgrodzicki.flashcards.grails.Lesson
 class BootStrap {
 
     def init = { servletContext ->
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+  
+		def testUser = new User(username: 'me', enabled: true, password: 'password')
+		testUser.save(flush: true)
+  
+		UserRole.create testUser, adminRole, true
+  
+		assert User.count() == 1
+		assert Role.count() == 2
+		assert UserRole.count() == 1
+  
     }
     def destroy = {
     }
