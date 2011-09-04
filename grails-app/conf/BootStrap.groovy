58b31118b4a1056eb5e13761291f034c9378753a
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-09-04 20:58:37 +0200

    changed default user/pass to admin/admin

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,7 +10,7 @@ class BootStrap {
 		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
 		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
   
-		def testUser = new User(username: 'me', enabled: true, password: 'password')
+		def testUser = new User(username: 'admin', enabled: true, password: 'admin')
 		testUser.save(flush: true)
   
 		UserRole.create testUser, adminRole, true
